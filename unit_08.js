f43e25acc740760ae20b71f48db22cb5caec5985
¨dim50dim¨ <zheliezkov@gmail.com>
2025-03-15 10:56:56 +0100

    remove === 0

--- unit_08.js
+++ unit_08.js
@@ -70,7 +70,7 @@ document.querySelector('.b-4').onclick = t4;
 function t5() {
     let out = '';
     for(let i = 1;i <= 17; i++){
-        if(i % 2 ) out += i + '_' + "**";
+        if(i % 2  ) out += i + '_' + "**";
         if(i % 2 !== 0) out += i + '_' + "*";
     }
     document.querySelector('.out-5').innerHTML = out;

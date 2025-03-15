b7c12d3f12f04f2e89fb3b1e68212b1c01a89de7
¨dim50dim¨ <zheliezkov@gmail.com>
2025-03-15 10:29:36 +0100

    in first task we have to show up row of numbers from 1 to 50 included

--- unit_08.js
+++ unit_08.js
@@ -5,7 +5,11 @@
 
 
 function t1() {
-
+    let out = '';
+    for(let i = 0; i <= 50; i++){
+        out += i + '_';
+    }
+    document.querySelector('.out-1').innerHTML = out;
 }
 
 document.querySelector('.b-1').onclick = t1;
